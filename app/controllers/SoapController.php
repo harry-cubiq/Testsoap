64f5e8f6ec5dd4112a551ceb672577c251750a29
Sebastián Velez <devsebastianvz@gmail.com>
2020-02-10 14:16:58 -0500

    soap systech

--- app/controllers/SoapController.php
+++ app/controllers/SoapController.php
@@ -11,18 +11,29 @@ class SoapController extends ControllerBase
     }
 
 
-    public function testSoapAction(){
-
-		$response  =  new Response();
-		$response->setContentType("application/json","UTF-8");
-		
-		$response->setStatusCode(200,"ok");
-		$response->setJsonContent([
-				"test"     => "Working",
-				"success" => true
-		]);
-		
-		return $response;
+    public function testSoapAction()
+    {
+        $wsdl   = "http://186.31.137.87/suite17/webservices/articulos.php?wsdl";
+        $client = new SoapClient($wsdl, array('login'=>"CUBIQ@6",'password'=>"CUBIQ123"));  
+        $client->__setLocation('http://186.31.137.87/suite17/webservices/articulos.php');
+        $request_param = array(
+           "arreglo_articulos" => array
+           (
+            "operacion" => "M",
+            "articulo" => "052740201567",
+            "integra_vocollect"=>""
+           )
+        );
+        
+        try
+        {
+            $responce_param = $client->validarArticulos($request_param);
+           //$responce_param =  $client->call("webservice_methode_name", $request_param); // Alternative way to call soap method
+        } 
+        catch (Exception $e) 
+        { 
+            return e;
+        }
     }
 
 }
